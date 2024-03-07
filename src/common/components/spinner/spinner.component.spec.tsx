708e7a188bae7af3a7f762d9353668a7d26f5618
Alber EE <122263897+Alber-Writer@users.noreply.github.com>
2024-03-07 15:47:32 +0100

    Add specs to spinner component

--- src/common/components/spinner/spinner.component.spec.tsx
+++ src/common/components/spinner/spinner.component.spec.tsx
@@ -1,13 +1,43 @@
-import { act, renderHook, waitFor } from '@testing-library/react';
+import React from 'react';
+import {
+  render,
+  screen,
+  within,
+} from '@testing-library/react';
 import { SpinnerComponent } from './spinner.component';
+//To avoid jest-and-babel known issues, we import directly the module below
+import * as promiseTracker from 'react-promise-tracker/lib/trackerHook';
 
 describe('common/components/spinner/spinner.component.tsx specs', () => {
-  it('', () => {
+  it('Should not render a modal when usePromiseTracker returns promiseInProgress:false', () => {
     // Arrange
+    const getStub = jest
+      .spyOn(promiseTracker, 'usePromiseTracker')
+      .mockReturnValue({ promiseInProgress: false });
 
     // Act
+    render(<SpinnerComponent />);
+    const modal = screen.queryByRole('presentation');
 
     // Assert
+    expect(getStub).toHaveBeenCalled();
+    expect(modal).not.toBeInTheDocument();
   });
 
+  it('Should render modal and loader when usePromiseTracker returns promiseInProgress:true', () => {
+    // Arrange
+    const getStub = jest
+      .spyOn(promiseTracker, 'usePromiseTracker')
+      .mockReturnValue({ promiseInProgress: true });
+
+    // Act
+    render(<SpinnerComponent />);
+    const modal = screen.queryByRole('presentation');
+    const loader = within(modal).queryByRole('loader');
+
+    // Assert
+    expect(getStub).toHaveBeenCalled();
+    expect(modal).toBeInTheDocument();
+    expect(loader).toBeInTheDocument();
+  });
 });
